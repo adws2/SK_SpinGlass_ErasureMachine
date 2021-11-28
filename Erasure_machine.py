0e0e70de03aeabb08c7c082206ff3360edebf441
JMJ <2002alswo@naver.com>
2021-11-29 04:10:50 +0900

    Change CUDA code

--- Erasure_machine.py
+++ Erasure_machine.py
@@ -9,12 +9,14 @@ import torch
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 class fit():
-    def __init__(self,epsilon = 0.7, learning_rate = 0.1, max_iter = 151,regularizer = 0.1):
+    def __init__(self,epsilon = 0.7, learning_rate = 0.1, max_iter = 151, device = 'cuda'):
         self.epsilon = epsilon
         self.learning_rate = learning_rate
         self.max_iter = max_iter
-        self.regularizer = regularizer
-        
+        self.device = 'cpu'
+        if torch.cuda.is_available() and device == 'cuda':
+            print('Use cuda')
+            self.device = 'cuda'
         
     def operators(self,configuration):
         #O = <S_i S_j>
@@ -28,32 +30,15 @@ class fit():
             jindex += system_size - index - 1
         return Op
         
-        
-    def em_cpu(self, ops):
+           
+    def em(self, ops, L1 = 0. , L2 = 0.):
         n_ops = ops.shape[0]
         np.random.seed(13)
         w = np.random.rand(n_ops)-0.5
-        for i in range(self.max_iter):
 
-            energies_w = w@ops
-
-            energies_max = energies_w.max()  
-            probs_w = np.exp((energies_w-energies_max)*(self.epsilon-1)) 
-            z_data = np.sum(probs_w)
-            probs_w /= z_data
-            ops_expect_w = np.sum(probs_w[np.newaxis,:]*ops,axis=1)
-
-
-            w += self.learning_rate*(ops_expect_w - w*self.epsilon - self.regularizer*w)
-
-        return w
-    
-    def em_gpu(self, ops):
-        n_ops = ops.shape[0]
-        np.random.seed(13)
-        w = np.random.rand(n_ops)-0.5
-        w = torch.from_numpy(w).cuda()
-        ops = torch.from_numpy(ops).cuda()
+        if self.device == 'cuda':
+            w = torch.from_numpy(w).cuda()
+            ops = torch.from_numpy(ops).cuda()
         
         for i in range(self.max_iter):
 
@@ -65,7 +50,7 @@ class fit():
             ops_expect_w = torch.sum(probs_w[np.newaxis,:]*ops,axis=1)
 
 
-            w += self.learning_rate*(ops_expect_w - w*self.epsilon - self.regularizer*w)
+            w += self.learning_rate*(ops_expect_w - w*self.epsilon - L1*w - L2*w /abs(w) )
 
         return w
 
